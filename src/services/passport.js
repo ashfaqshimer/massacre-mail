f7188a0244a862162c9fe27f165fdc88d019062c
ashfaqshimer <shimer.ahamed@gmail.com>
2019-09-01 23:40:09 +0530

    Tweak google strategy settings

--- src/services/passport.js
+++ src/services/passport.js
@@ -22,7 +22,8 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		async (accessToken, refreshToken, profile, cb) => {
 			const userId = profile.id;
