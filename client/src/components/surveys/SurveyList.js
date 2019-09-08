a1f39d8383f0ae0097bcadfd098396ea9e0700eb
ashfaqshimer <shimer.ahamed@gmail.com>
2019-09-08 14:18:43 +0530

    Add a message if there are no surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -9,24 +9,37 @@ const SurveyList = ({ surveys, fetchSurveys }) => {
 
 	return (
 		<div>
-			{surveys.reverse().map(survey => {
-				return (
-					<div key={survey._id} className='card darken-1'>
-						<div className='card-content text-white'>
-							<span className='card-title'>{survey.title}</span>
-							<p>{survey.body}</p>
-							<p className='right'>
-								Date Sent:{' '}
-								{new Date(survey.dateSent).toLocaleDateString()}{' '}
-							</p>
+			{surveys.length === 0 ? (
+				<>
+					<h3 className='center'>You have not created any surveys</h3>
+					<p className='center'>
+						CLick on the + button below to create a new survey.
+					</p>
+				</>
+			) : (
+				surveys.reverse().map(survey => {
+					return (
+						<div key={survey._id} className='card darken-1'>
+							<div className='card-content text-white'>
+								<span className='card-title'>
+									{survey.title}
+								</span>
+								<p>{survey.body}</p>
+								<p className='right'>
+									Date Sent:{' '}
+									{new Date(
+										survey.dateSent
+									).toLocaleDateString()}{' '}
+								</p>
+							</div>
+							<div className='card-action'>
+								<a>Yes Votes: {survey.yes}</a>
+								<a>No Votes: {survey.no}</a>
+							</div>
 						</div>
-						<div className='card-action'>
-							<a>Yes Votes: {survey.yes}</a>
-							<a>No Votes: {survey.no}</a>
-						</div>
-					</div>
-				);
-			})}
+					);
+				})
+			)}
 		</div>
 	);
 };
